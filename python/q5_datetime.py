e7008ea7f47c827025540e1c72aeb1ae3ccfe06d
Sameh Saleh <sameh.n.saleh@gmail.com>
2016-06-16 20:50:10 -0400

    completed q5_datetime

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,26 @@
 # Hint:  use Google to find python function
+from datetime import datetime, date
 
 ####a) 
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'
+fmt = '%m-%d-%Y' 
+start = datetime.strptime(date_start,fmt)
+stop = datetime.strptime(date_stop,fmt)
+print (stop-start).days
 
 ####b)  
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015' 
+fmt = '%m%d%Y' 
+start = datetime.strptime(date_start,fmt)
+stop = datetime.strptime(date_stop,fmt)
+print (stop-start).days
 
 ####c)  
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015' 
+fmt = '%d-%b-%Y' 
+start = datetime.strptime(date_start,fmt)
+stop = datetime.strptime(date_stop,fmt)
+print (stop-start).days
