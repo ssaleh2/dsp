0e5c05b25f04b9bfaf1be951c1d2c7614dcd881e
Sameh Saleh <sameh.n.saleh@gmail.com>
2016-06-18 18:23:39 -0400

    Completed advanced python csv @sampathweb

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,20 @@
-PLACE YOUR CODE HERE
+import csv
+
+def email_list(data):
+	"""read csv file and return email list"""
+	faculty = []
+  	file = open(data, 'rb')
+  	reader = csv.reader(file)
+  	faculty = list(reader)[1:]
+  	emails = [i[3] for i in faculty]
+  	return emails
+
+def email_write_csv(email_list):
+	"""write emails out to csv file"""
+	with open('email.csv', 'wrb') as file:
+		writer = csv.writer(file)
+		for i in email_list:
+			writer.writerow([i])
+
+emails = email_list('faculty.csv')
+email_write_csv(emails)
